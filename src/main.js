bebbacd0fc8b496c3a70c6e7e21f4d32635bf49c
juank <juanprogram06@gmail.com>
2023-05-10 16:20:19 -0500

    Uso de FormData dentro la carga del formulario con la Data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 
-const url_Api_random = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS';
-const api_url_favorites = 'https://api.thecatapi.com/v1/favourites?api_key=live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS';
-const api_url_favorites_delete = (id)=> `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS`;
+const url_Api_random = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const api_url_favorites = 'https://api.thecatapi.com/v1/favourites';
+const api_url_favorites_delete = (id)=> `https://api.thecatapi.com/v1/favourites/${id}`;
+const api_url_upload = 'https://api.thecatapi.com/v1/images/upload';
 const spanError = document.getElementById('error');
 const buttonChange = document.getElementById('change');
 
@@ -29,7 +30,12 @@ async function changeTheCat(){
 }
 
 async function loadFavourites(){
-    const favourites = await fetch(api_url_favorites);
+    const favourites = await fetch(api_url_favorites,{
+        method: 'GET',
+        headers:{
+            'X-API-KEY':'live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS'
+        }
+    });
     const data = await favourites.json();
     console.log('Fav');
     console.log(data);
@@ -68,6 +74,7 @@ async function saveFavouriteCat(id){
         method: 'POST',
         headers: {
             'Content-Type':'application/json',
+            'X-API-KEY':'live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS'
         },
         body: JSON.stringify({
             image_id: id
@@ -84,22 +91,53 @@ async function saveFavouriteCat(id){
 }
 
 async function deleteFavouriteCat(id){
-    const res = await fetch(api_url_favorites_delete(id),{
-        method: 'DELETE',
-        headers: {
-            'Content-Type':'application/json',
-        },
-    });
-    console.log(res);
-    const data = await res.json();
-    console.log(data);
-
-    if(res.status !== 200){
-        spanError.innerHTML= `Hubo un error ${res.status} ${data.message}`;
-    }else{
+    try{
+        const res = await fetch(api_url_favorites_delete(id),{
+            method: 'DELETE',
+            headers: {
+                'Content-Type':'application/json',
+                'X-API-KEY':'live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS'
+            },
+        });
+        const data = await res.json();
+        console.log(data);
         console.log('Gato eliminado en favoritos');
         loadFavourites();
     }
+    catch{
+        const res = await fetch(api_url_favorites_delete(id),{
+            method: 'DELETE',
+            headers: {
+                'Content-Type':'application/json',
+            },
+        });
+        
+        if(res.status !== 200){
+            spanError.innerHTML= `Hubo un error ${res.status} `;
+        }
+    }
+
+    
+}
+
+async function uploadMichiPhoto(){
+    const form = document.getElementById('uploadForm');
+    //FormData es una clase la cual nos permite crear objetos a partir de formularios de google
+    //Con el objetivo de que sea mas facil el poder envar estos por una API, claramente en forma de objetos
+    const formData = new FormData(form);
+    console.log(formData.get('file'));
+
+    const res = await fetch(api_url_upload,{
+        method:'POST',
+        headers:{
+            //Al ser enciado en el body el una instancia de FormData no necesitamos enviar un header de content type
+            //'Content-Type':'',
+            'X-API-KEY':'live_rfYQ4wXDI7f3lwl4vM5VuXXeD3aNbUV6Pu7I9gMwP5bAVg2W9IFfhUeH3Xvxo4jS'
+        },
+        //Al ser enciado en el body el una instancia de FormData no necesitamos hacer un JSON.stringify de nuestro objeto
+        body:formData,
+    })
+
 }
 changeTheCat();
 loadFavourites();
